94709a39828029df22d2d669e92a6e718d21d70a
Tarra Ghaffari <tarrag2525@gmail.com>
2023-11-27 16:46:29 -0800

    Set up server

--- server/index.js
+++ server/index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
+const cors = require ('cors')
 
-app.listen(5000, () => {
-    console.log("Server has started on port 5000")
+//middleware
+app.use(cors())
+
+app.listen(3000, () => {
+    console.log("Server has started on port 3000")
 });
