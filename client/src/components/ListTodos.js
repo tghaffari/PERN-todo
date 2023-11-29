9a6826f25db00fbb57415111e5740034f0bd5688
Tarra Ghaffari <tarrag2525@gmail.com>
2023-11-29 13:10:33 -0800

    Added delete functionality

--- client/src/components/ListTodos.js
+++ client/src/components/ListTodos.js
@@ -5,6 +5,19 @@ const ListTodos = () => {
 
     const [todos, setTodos] = useState([]);
 
+    //delete function 
+
+    const deleteTodo = async(id) => {
+        try {
+            const deleteTodo = await fetch (`http://localhost:3000/todos/${id}`, {
+                method: "DELETE"
+            });
+            setTodos(todos.filter(todo => todo.todo_id !== id))
+        } catch (err) {
+            console.error(err.message)
+        }
+    }
+
     const getTodos = async () => {
         try {
             const response = await fetch("http://localhost:3000/todos")
@@ -40,10 +53,10 @@ const ListTodos = () => {
                     <td>john@example.com</td>
                 </tr> */}
                 {todos.map(todo => (
-                    <tr>
+                    <tr key={todo.todo_id}>
                         <td>{todo.description}</td>
                         <td>Edit</td>
-                        <td>Delete</td>
+                        <td><button className="btn btn-danger" onClick={() => deleteTodo(todo.todo_id)}>Delete</button></td>
                     </tr>
                 ) )}
                 </tbody>
