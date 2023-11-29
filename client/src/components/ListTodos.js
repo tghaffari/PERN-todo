0b671e50ba9db5681cebcec5402cea432d80e987
Tarra Ghaffari <tarrag2525@gmail.com>
2023-11-29 15:25:08 -0800

    Added edit functionality

--- client/src/components/ListTodos.js
+++ client/src/components/ListTodos.js
@@ -1,4 +1,5 @@
 import React, {useEffect, useState} from "react";
+import EditTodo from "./EditTodo";
 
 
 const ListTodos = () => {
@@ -55,7 +56,7 @@ const ListTodos = () => {
                 {todos.map(todo => (
                     <tr key={todo.todo_id}>
                         <td>{todo.description}</td>
-                        <td>Edit</td>
+                        <td><EditTodo todo = {todo} /></td>
                         <td><button className="btn btn-danger" onClick={() => deleteTodo(todo.todo_id)}>Delete</button></td>
                     </tr>
                 ) )}
